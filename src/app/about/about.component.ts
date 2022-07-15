ee9da507f0a82f83536c74237f370246a17fd1e4
Koushik-Deb <koushik.imon@gmail.com>
2022-07-15 18:23:59 +0600

    "Added my project with example of callback hell"

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    document.addEventListener('click',evt=>{
+      console.log(evt);
+
+      
+    setTimeout(()=>{
+      console.log("Set Timeout is Done");
+
+      let counter = 0
+      setInterval(()=>{
+        console.log(counter);
+        counter++;
+      },1000)
+    
+    },3000)
+
+    })
+
+
+  }
+
+}
